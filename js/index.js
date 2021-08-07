55364a3ab53ace4b4191776044215063e0b0cbbd
ambrlei <80174813+ambrlei@users.noreply.github.com>
2021-08-07 15:59:29 -0600

    Hamburger menu
    Currently working on the opening state of the hamburger menu. The hamburger open menu is created, but jquery is not being noticed on the browser. Trying to figure it out

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 console.log("Your index.js file is loaded correctly!");
 
 
-$(".toggle").click(function(){
 
-    $(this).find(".toggleContainer").toggleClass("active");
-    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+$(".hamburgerOpen").hide();
+
+
+$(".hamburger").click(function() {
+    $(".hamburgerOpen").slideToggle("slow");
 });
